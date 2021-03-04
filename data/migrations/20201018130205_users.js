2d78059df18c612f064d2b4b7f96017cca1db8a0
Cory Thomas <cory.a.thomas1992@gmail.com>
2021-03-04 12:28:41 -0500

    updated schema's to include tasks and notes and also edited the plants table some. Also made some changes to the seeds, and created new files and folders for the notes and tasks.

--- data/migrations/20201018130205_users.js
+++ data/migrations/20201018130205_users.js
@@ -1,21 +1,54 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('users', (tbl) => {
+      tbl.increments();
+      tbl.string('username', 128).notNullable().unique().index();
+      tbl.string('password', 128).notNullable();
+    })
+    .createTable('plants', (tbl) => {
+      tbl.increments();
+      tbl.string('nickname', 128).notNullable().index();
+      tbl.string('species', 128);
+      tbl.string('image_url');
+      tbl.dateTime('date');
+      tbl
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('users.id')
+        .onDelete('CASCADE');
+    })
+    .createTable('notes', (tbl) => {
+      tbl.increments();
+      tbl.string('title', 128).notNullable().index();
+      tbl.string('body', 500).notNullable();
+      tbl.dateTime('date');
+      tbl
+        .integer('plant_id')
+        .unsigned()
+        .notNullable()
+        .references('plants.id')
+        .onDelete('CASCADE');
+    })
+    .createTable('tasks', (tbl) => {
+      tbl.increments();
+      tbl.string('title', 128).notNullable().index();
+      tbl.string('body', 500).notNullable();
+      tbl.integer('h2o_frequency');
+      tbl.dateTime('date');
+      tbl
+        .integer('plant_id')
+        .unsigned()
+        .notNullable()
+        .references('plants.id')
+        .onDelete('CASCADE');
+    });
+};
 
-exports.up = function(knex) {
-    return knex.schema
-      .createTable("users", tbl => {
-          tbl.increments(); 
-          tbl.string("username", 128).notNullable().unique().index();
-          tbl.string("password", 128).notNullable(); 
-      })
-      .createTable("plants", tbl => {
-          tbl.increments();
-          tbl.string("nickname", 128).notNullable().index();
-          tbl.string("species", 128)
-          tbl.integer('h2o_frequency').notNullable();
-          tbl.integer("user_id").unsigned().notNullable().references("users.id").onDelete('CASCADE');
-          tbl.string('image_url');
-      })
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTableIfExists("plants").dropTableIfExists("users")
-  };
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('notes')
+    .dropTableIfExists('plants')
+    .dropTableIfExists('users');
+};
--- data/seeds/002plants2.js
+++ data/seeds/002plants2.js
@@ -4,28 +4,31 @@ exports.seed = function (knex) {
     .then(function () {
       return knex('plants').insert([
         {
-          id: 1, 
-          nickname: "Aloe Vera",
-          species: "Aloe Vera",
-          h2o_frequency: 14,
+          id: 1,
+          nickname: 'Aloe Vera',
+          species: 'Aloe Vera',
+          image_url:
+            'https://images.unsplash.com/photo-1567689265664-1c48de61db0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=976&q=80',
+          date: date.setTime(date.getTime() + days * 86400000),
           user_id: 1,
-          image_url: 'https://images.unsplash.com/photo-1567689265664-1c48de61db0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=976&q=80'
         },
         {
-          id: 2, 
-          nickname: "Desert Rose",
-          species: "Adenium Obesium",
-          h2o_frequency: 7,
+          id: 2,
+          nickname: 'Desert Rose',
+          species: 'Adenium Obesium',
+          image_url:
+            'https://images.unsplash.com/photo-1586170112425-3adf1ed0146e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+          date: date.setTime(date.getTime() + days * 86400000),
           user_id: 1,
-          image_url: 'https://images.unsplash.com/photo-1586170112425-3adf1ed0146e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
         },
         {
-          id: 3, 
-          nickname: "Boston Fern",
-          species: "Nephrolepis Exaltata",
-          h2o_frequency: 3,
+          id: 3,
+          nickname: 'Boston Fern',
+          species: 'Nephrolepis Exaltata',
+          image_url:
+            'https://www.plantshop.me/media/product/89280-00-bakie_20190222085524.jpg',
+          date: date.setTime(date.getTime() + days * 86400000),
           user_id: 1,
-          image_url: 'https://www.plantshop.me/media/product/89280-00-bakie_20190222085524.jpg'
         },
       ]);
     });
